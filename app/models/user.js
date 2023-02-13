b155981ba7f2edff4a0e75a0a2c06406efe9badc
isobelmorales <isofmorales@gmail.com>
2023-02-13 17:24:40 -0500

    fixed error in user model and added routes to server.js

--- app/models/user.js
+++ app/models/user.js
@@ -2,27 +2,25 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema(
     {
-        {
-            email: {
-                type: String,
-                required: true,
-                unique: true,
-            },
-            hashedPassword: {
-                type: String,
-                required: true,
-            },
-            token: String,
+        email: {
+            type: String,
+            required: true,
+            unique: true,
         },
-        {
-            timestamps: true,
-            toObject: {
-                transform: (_doc, user) => {
-                    delete user.hashedPassword
-                    return user
-                },
+        hashedPassword: {
+            type: String,
+            required: true,
+        },
+        token: String,
+    },
+    {
+        timestamps: true,
+        toObject: {
+            transform: (_doc, user) => {
+                delete user.hashedPassword
+                return user
             },
-        }
+        },
     }
 )
 
