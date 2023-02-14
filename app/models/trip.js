bfa160bf646f15ff71bf9f438848dac5c706f1ee
isobelmorales <isofmorales@gmail.com>
2023-02-14 17:24:26 -0500

    added min/max temperature virtuals that call on weather api to trip model

--- app/models/trip.js
+++ app/models/trip.js
@@ -38,6 +38,46 @@ const tripSchema = new mongoose.Schema(
 
 //// VIRTUALS ////
 
+tripSchema.virtual('minTemperature').get(async function() {
+    // constructing the weather API URL using the trip's city and our OpenWeatherMap API key
+    const weatherApiKey = 'dc2845f9bd781af2272c91ca53a8aabf'
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${weatherApiKey}`
+
+    // makes an API request using fetch to get the weather data for the city
+    const response = await fetch(weatherApiUrl)
+    const data = await response.json()
+
+    // filtering the data to only return info for the dates between the start and end date
+    const filteredData = data.list.filter(weatherData => {
+        const date = new Date(weatherData.dt_txt)
+        return date >= this.startDate && date <= this.endDate
+    })
+
+    // returning the minimum temperature from the filtered weather data
+    const temperatures = filteredData.map(weatherData => weatherData.main.temp)
+    return Math.min(...temperatures)
+})
+  
+tripSchema.virtual('maxTemperature').get(async function() {
+    // constructing the weather API URL using the trip's city and our OpenWeatherMap API key
+    const weatherApiKey = 'dc2845f9bd781af2272c91ca53a8aabf'
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${weatherApiKey}`
+
+    // makes an API request using fetch to get the weather data for the city
+    const response = await fetch(weatherApiUrl)
+    const data = await response.json()
+
+    // filtering the data to only return info for the dates between the start and end date
+    const filteredData = data.list.filter(weatherData => {
+        const date = new Date(weatherData.dt_txt)
+        return date >= this.startDate && date <= this.endDate
+    })
+
+    // returning the maximum temperature from the filtered weather data
+    const temperatures = filteredData.map(weatherData => weatherData.main.temp)
+    return Math.max(...temperatures)
+})
+
 
 //// EXPORT ////
 
