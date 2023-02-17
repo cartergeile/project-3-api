e6e5e1142f6aa2e06a35d913a3a1875140769499
isobelmorales <isofmorales@gmail.com>
2023-02-17 10:39:26 -0500

    updated virtuals and corrected trip seed

--- app/models/TripSeed.js
+++ app/models/TripSeed.js
@@ -5,12 +5,12 @@ const Trip = require('./trip')
 const db = require('../../config/db')
 
 const startTrips = [
-  {location: 'Mediterranean', city: 'Monacco', startDate: new Date (2023, 01, 20), endDate: new Date (2023, 01, 27) },
-  {location: 'Caribbean', city:'Havana', startDate: new Date (2023, 02, 20), endDate: new Date (2023, 02, 27) },
-  {location: 'Indian Ocean', city: 'Aden', startDate: new Date (2023, 03, 20), endDate: new Date (2023, 03, 27) },
-  {location: 'Antarctica', city: 'Port Lockroy', startDate: new Date (2023, 04, 20), endDate: new Date (2023, 04, 27) },
-  {location: 'Far East', city: 'Vladivostok', startDate: new Date (2023, 05, 20), endDate: new Date (2023, 05, 27) },
-  {location: 'Mississippi River', city:'St. Louis', startDate: new Date (2023, 06, 20), endDate: new Date (2023, 06, 27) }
+  {location: 'Mediterranean', city: 'Monte Carlo', startDate: new Date('2023-01-20'), endDate: new Date('2023-01-27') },
+  {location: 'Caribbean', city:'Havana', startDate: new Date('2023-02-20'), endDate: new Date('2023-02-27') },
+  {location: 'Indian Ocean', city: 'Aden', startDate: new Date('2023-03-20'), endDate: new Date('2023-03-27') },
+  {location: 'Antarctica', city: 'Port Lockroy', startDate: new Date('2023-04-20'), endDate: new Date('2023-04-27') },
+  {location: 'Far East', city: 'Vladivostok', startDate: new Date('2023-05-20'), endDate: new Date('2023-05-27') },
+  {location: 'Mississippi River', city:'St. Louis', startDate: new Date('2023-06-20'), endDate: new Date('2023-06-27') }
 ]
 
 mongoose.connect(db, {
--- app/models/trip.js
+++ app/models/trip.js
@@ -37,47 +37,24 @@ const tripSchema = new mongoose.Schema(
 )
 
 //// VIRTUALS ////
-
-tripSchema.virtual('minTemperature').get(async function() {
-    // constructing the weather API URL using the trip's city and our OpenWeatherMap API key
-    const weatherApiKey = 'dc2845f9bd781af2272c91ca53a8aabf'
-    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${weatherApiKey}`
-
-    // makes an API request using fetch to get the weather data for the city
-    const response = await fetch(weatherApiUrl)
-    const data = await response.json()
-
-    // filtering the data to only return info for the dates between the start and end date
-    const filteredData = data.list.filter(weatherData => {
-        const date = new Date(weatherData.dt_txt)
-        return date >= this.startDate && date <= this.endDate
-    })
-
-    // returning the minimum temperature from the filtered weather data
-    const temperatures = filteredData.map(weatherData => weatherData.main.temp)
-    return Math.min(...temperatures)
+tripSchema.virtual('cityid').get(function (){
+    switch(this.city) {
+        case 'Monte Carlo':
+            return '2992741';
+        case 'Havana':
+            return '3553478';
+        case 'Aden':
+            return '415189';
+        case 'Port Lockroy':
+            return 'n/a';
+        case 'Vladivostok':
+            return '2013348';
+        case 'St. Louis':
+            return '4407066';
+        default:
+            return 'n/a'
+    }
 })
-  
-tripSchema.virtual('maxTemperature').get(async function() {
-    // constructing the weather API URL using the trip's city and our OpenWeatherMap API key
-    const weatherApiKey = 'dc2845f9bd781af2272c91ca53a8aabf'
-    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${weatherApiKey}`
-
-    // makes an API request using fetch to get the weather data for the city
-    const response = await fetch(weatherApiUrl)
-    const data = await response.json()
-
-    // filtering the data to only return info for the dates between the start and end date
-    const filteredData = data.list.filter(weatherData => {
-        const date = new Date(weatherData.dt_txt)
-        return date >= this.startDate && date <= this.endDate
-    })
-
-    // returning the maximum temperature from the filtered weather data
-    const temperatures = filteredData.map(weatherData => weatherData.main.temp)
-    return Math.max(...temperatures)
-})
-
 
 //// EXPORT ////
 
